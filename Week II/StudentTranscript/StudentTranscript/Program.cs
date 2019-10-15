3314c71a29dd642cf630d3f546397e85faf4445f
Roni98 <rd27240@seeu.edu.mk>
2019-10-15 21:38:37 +0200

    Week 2 homework

--- Week II/StudentTranscript/StudentTranscript/Program.cs
+++ Week II/StudentTranscript/StudentTranscript/Program.cs
@@ -1,14 +1,15 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace StudentTranscript
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string[] courses = new string[10];
-            int[] credits = new int[10];
-            int[] grades = new int[10];
+
+            List<string> courses = new List<string>();
+            List<int> credits = new List<int>();
+            List<int> grades = new List<int>();
             int currentIndex = 0;
             while (true)
             {
@@ -19,16 +20,29 @@ namespace StudentTranscript
                 {
                     break;
                 }
-                courses[currentIndex] = courseName;
+                courses.Add(courseName);
                 int grade, credit;
                 Console.WriteLine("Please register the grade for course:  " + courseName);
                 grade = Convert.ToInt32(Console.ReadLine());
+               
+                
+                while ((grade != -2 || grade != -1) && grade < 5 && grade > 10)
+                {
+                    Console.WriteLine("Invalid grade entered:  " + courseName);
+                    grade = Convert.ToInt32(Console.ReadLine());
+                  
+                }
 
                 Console.WriteLine("Please register the credits for course:  " + courseName);
                 credit = Convert.ToInt32(Console.ReadLine());
+                while (credit < 1 || credit > 10)
+                {
+                    Console.WriteLine("Invalid credits entered:  " + courseName);
+                    credit = Convert.ToInt32(Console.ReadLine());
+                }
 
-                credits[currentIndex] = credit;
-                grades[currentIndex] = grade;
+                credits.Add(credit);
+                grades.Add(grade);
                 currentIndex++;
             }
 
@@ -37,20 +51,36 @@ namespace StudentTranscript
             Console.WriteLine("COURSE NAME \t CREDITS \t GRADE");
             Console.WriteLine("=====================================");
 
-            double sum = 0, sumOfCredits = 0;
+            double sum = 0, sumOfCredits = 0, creditsPassed = 0;
             for (int i = 0; i < currentIndex; i++)
             {
-                Console.WriteLine(courses[i] + "\t" + credits[i] + "\t" + grades[i]);
-
+                if (grades[i] < 0)
+                {
+                    if(grades[i] == -1)
+                        Console.WriteLine(courses[i] + "\t" + credits[i] + "\t" + "IN");
+                    else
+                        Console.WriteLine(courses[i] + "\t" + credits[i] + "\t" + "NR");
+                }
+                else
+                    Console.WriteLine(courses[i] + "\t" + credits[i] + "\t" + grades[i]);
+                if(grades[i] > 5)
+                {
+                    creditsPassed += credits[i];
+                    sum += grades[i] * credits[i];
+                }
                 sumOfCredits += credits[i];
-                sum += grades[i] * credits[i];
+                
             }
-            double gpa = sum / sumOfCredits;
+            double gpa = sum / creditsPassed;
 
             Console.WriteLine("=====================================");
+            Console.WriteLine("Credits attempted: " + sumOfCredits);
+            Console.WriteLine("Credits awarded: " + creditsPassed);
+            Console.WriteLine("Number of courses: " + currentIndex);
             Console.WriteLine("GPA: " + gpa.ToString("0.00"));
 
             Console.ReadLine();
         }
     }
+}
 }
